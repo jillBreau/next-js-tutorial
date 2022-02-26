529c8a6bdce14a9ca890e4b6e8de527aa5b29151
Jillian Breau <jmbreau@mun.ca>
2022-02-26 13:12:05 -0330

    chore: convert to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 import Layout, { siteTitle } from '../components/layout';
 import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-import useSWR from 'swr';
+import useSWR, { Fetcher } from 'swr';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
@@ -15,7 +16,7 @@ export async function getStaticProps() {
   };
 };
 
-const fetcher = (...args) => fetch(...args).then(res => res.json());
+const fetcher: Fetcher<{text: string}, RequestInfo> = (...args: Parameters<typeof fetch>) => fetch(...args).then(res => res.json());
 
 export default function Home({ allPostsData }) {
   const { data, error } = useSWR('/api/get-word', fetcher);
@@ -27,12 +28,12 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Jillian and I'm learning Next.js.</p>
+        <p>Hi, I'm Jillian and I'm learning Next.js using their <a href="https://nextjs.org/learn">tutorial</a>.</p>
         <p>
           Here are some topics I've learned about (written by the Next.js team).
         </p>
         <p>
-          I also added a step to get this <em>{word}</em> from an API.
+          I've also added a step to get this <em>{word}</em> from an API, and have converted the result of the basic tutorial into TypeScript.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
